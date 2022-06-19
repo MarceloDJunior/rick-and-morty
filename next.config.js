f433f854a6824d19f86f18878352429c689869c7
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-06-19 16:16:44 -0300

    chore: add next react svg

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
+const withReactSvg = require('next-react-svg');
+
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withReactSvg({
+  ...nextConfig,
+  include: /\.svg$/,
+});
