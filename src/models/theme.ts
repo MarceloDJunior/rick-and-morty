fc12e6385788257714b94ede502960ee62dad5af
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-06-25 15:38:00 -0300

    feat: add toxic theme

--- src/models/theme.ts
+++ src/models/theme.ts
@@ -1,9 +1,11 @@
 export enum Theme {
   DEFAULT = 'default',
   DARK = 'dark',
+  TOXIC = 'toxic',
 }
 
 export const ThemePrimaryColor = {
   [Theme.DEFAULT]: '#0c8498',
   [Theme.DARK]: '#000',
+  [Theme.TOXIC]: '#1ad414',
 };
