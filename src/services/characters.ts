1487c93b13ae5c67efd51be950d6728351777740
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-06-25 21:23:58 -0300

    chore: remove catch from service call

--- src/services/characters.ts
+++ src/services/characters.ts
@@ -14,38 +14,30 @@ export type GetCharactersResponse = {
 
 export class CharactersService {
   public static async getCharacters(page = 1): Promise<GetCharactersResponse> {
-    try {
-      const response = await api.get(`/character/?page=${page}`);
-      switch (response.status) {
-        case 200:
-          return response.data as GetCharactersResponse;
-        case 404:
-          throw new NotFoundError();
-        default:
-          throw new UnexpectedError();
-      }
-    } catch (error) {
-      throw new UnexpectedError();
+    const response = await api.get(`/character/?page=${page}`);
+    switch (response.status) {
+      case 200:
+        return response.data as GetCharactersResponse;
+      case 404:
+        throw new NotFoundError();
+      default:
+        throw new UnexpectedError();
     }
   }
 
   public static async getCharactersByName(name: string, page = 1): Promise<GetCharactersResponse> {
-    try {
-      let endpoint = `/character/?page=${page}`;
-      if (name) {
-        endpoint += `&name=${name}`;
-      }
-      const response = await api.get(endpoint);
-      switch (response.status) {
-        case 200:
-          return response.data as GetCharactersResponse;
-        case 404:
-          throw new NotFoundError();
-        default:
-          throw new UnexpectedError();
-      }
-    } catch (error) {
-      throw new UnexpectedError();
+    let endpoint = `/character/?page=${page}`;
+    if (name) {
+      endpoint += `&name=${name}`;
+    }
+    const response = await api.get(endpoint);
+    switch (response.status) {
+      case 200:
+        return response.data as GetCharactersResponse;
+      case 404:
+        throw new NotFoundError();
+      default:
+        throw new UnexpectedError();
     }
   }
 }
