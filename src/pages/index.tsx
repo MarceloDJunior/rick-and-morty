5db3d5100412bc2c6c7c190f9883ff7de62f4d88
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-07-02 20:31:53 -0300

    feat: add themed status bar color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
+import { ThemePrimaryColor } from '@/models/theme';
+import { useThemeContext } from '@/contexts/theme-context';
 import { Characters } from '@/views/characters';
 
 const HomePage: NextPage = () => {
-  return <Characters />;
+  const { theme } = useThemeContext();
+
+  return (
+    <>
+      <Head>
+        <meta name="theme-color" content={ThemePrimaryColor[theme]} />
+      </Head>
+      <Characters />
+    </>
+  );
 };
 
 export default HomePage;
