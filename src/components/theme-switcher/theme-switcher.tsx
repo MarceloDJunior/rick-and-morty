f42e29905ffca80d230f1cee5b408f0114b5205d
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-07-23 22:45:35 -0300

    chore: add transition when switching themes

--- src/components/theme-switcher/theme-switcher.tsx
+++ src/components/theme-switcher/theme-switcher.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 
 import { useThemeContext } from '@/contexts/theme-context';
@@ -8,8 +8,9 @@ import { useOnClickOutside } from '@/hooks/use-on-click-outside';
 import styles from './theme-switcher.module.scss';
 
 export const ThemeSwitcher = () => {
-  const { theme, changeTheme } = useThemeContext();
+  const { theme: selectedTheme, changeTheme } = useThemeContext();
   const [isOpen, setIsOpen] = useState(false);
+  const [isChangingTheme, setIsChangingTheme] = useState(false);
   const popupRef = useRef<HTMLDivElement>(null);
 
   const close = useCallback(() => {
@@ -20,33 +21,48 @@ export const ThemeSwitcher = () => {
 
   const updateTheme = useCallback(
     (theme: Theme) => {
-      changeTheme(theme);
-      close();
+      document.body.classList.add('changing-theme');
+      setTimeout(() => {
+        setIsChangingTheme(true);
+        changeTheme(theme);
+        close();
+      }, 400);
     },
     [close, changeTheme]
   );
 
+  useEffect(() => {
+    if (isChangingTheme) {
+      setIsChangingTheme(false);
+      document.body.classList.remove('changing-theme');
+    }
+  }, [isChangingTheme]);
+
   return (
-    <div className={styles.container}>
-      <button type="button" className={styles.switcher} onClick={() => setIsOpen(true)}>
-        <div className={styles.theme}>{theme}</div>
-        <div className={styles.color} style={{ backgroundColor: ThemePrimaryColor[theme] }}></div>
-      </button>
-      {isOpen && (
-        <div className={styles['theme-popup']} ref={popupRef} onBlur={close}>
+    <div className={classNames(styles.container, isOpen ? styles.open : styles.closed)}>
+      {isOpen ? (
+        <div className={styles['theme-list']} ref={popupRef} onBlur={close}>
           <h4 className={styles.title}>Select a theme</h4>
-          {Object.values(Theme).map(t => (
+          {Object.values(Theme).map(theme => (
             <button
               type="button"
-              className={classNames(styles.item, { [styles.active]: t === theme })}
-              key={t}
-              onClick={() => updateTheme(t)}
+              className={classNames(styles.item, { [styles.active]: theme === selectedTheme })}
+              key={theme}
+              onClick={() => updateTheme(theme)}
             >
-              <div className={styles.theme}>{t}</div>
-              <div className={styles.color} style={{ backgroundColor: ThemePrimaryColor[t] }}></div>
+              <div className={styles.theme}>{theme}</div>
+              <div className={styles.color} style={{ backgroundColor: ThemePrimaryColor[theme] }} />
             </button>
           ))}
         </div>
+      ) : (
+        <button type="button" className={styles.switcher} onClick={() => setIsOpen(true)}>
+          <div className={styles.theme}>{selectedTheme}</div>
+          <div
+            className={styles.color}
+            style={{ backgroundColor: ThemePrimaryColor[selectedTheme] }}
+          />
+        </button>
       )}
     </div>
   );
